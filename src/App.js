e05cd17b426abfbab9015ee776954f390aa3d198
Arun Chapagain <arunsansar123@gmail.com>
2020-05-30 19:02:27 +0545

    Use effect

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import ClassCounter from './components/ClassCounter'
 import HookCounter from './components/HookCounter'
+import HookCounterOne from './components/HookCounterOne'
 import HookCounterTwo from './components/HookCounterTwo'
 import HookCounterThree from './components/HookCounterThree'
 import HookCounterFour from './components/HookCounterFour'
+
 function App() {
   return (
     <div className="App">
@@ -12,7 +14,8 @@ function App() {
       // <HookCounter/> */}
       {/* <HookCounterTwo/> */}
       {/* <HookCounterThree/> */}
-      <HookCounterFour/>
+      {/* // <HookCounterFour/> */}
+      <HookCounterOne/>
 
     </div>
   );
---
+++ src/components/HookCounterOne.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+
+const HookCounterOne = () => {
+    const [count, setCount] = useState(0)
+
+
+  useEffect (()=>{
+    document.title = `click ${count} times`
+  })
+
+
+  return (
+    <div>
+        <button onClick={()=>setCount(count+1)}>Click {count} times</button>
+      
+    </div>
+  )
+}
+
+export default HookCounterOne
